6ddabfacad33c4e7f9274a1f79c23b16232119c5
Zohar <neuzohar@gmail.com>
2025-06-23 11:18:09 +0100

    create relationship between country and product codes and corresponding names and descriptions in orm model

--- app/models/trade.py
+++ app/models/trade.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, Float, PrimaryKeyConstraint
+from sqlalchemy import Column, Integer, Float, PrimaryKeyConstraint, ForeignKey
+from sqlalchemy.orm import relationship
 # from app.database.database import Base
 from database.database import Base
 
@@ -6,13 +7,17 @@ class TradeFlow(Base):
     __tablename__ = "trade_flows"
 
     year = Column(Integer, index=True)
-    exporter = Column(Integer, index=True)
-    importer = Column(Integer, index=True)
-    product_code = Column(Integer, index=True)
+    exporter_id = Column(Integer, ForeignKey("country_codes.country_code"), index=True)
+    importer_id = Column(Integer, ForeignKey("country_codes.country_code"), index=True)
+    product_code = Column(Integer, ForeignKey("product_codes.code"), index=True)
     value = Column(Float, index=True)
     quantity = Column(Float, index=True)
 
+    exporter=relationship("Country", foreign_keys = [exporter_id])
+    importer=relationship("Country", foreign_keys = [importer_id])
+    product_description=relationship("Product", foreign_keys = [product_code])
+
     __table_args__ = (
-        PrimaryKeyConstraint(year, exporter, importer, product_code),
+        PrimaryKeyConstraint(year, exporter_id, importer_id, product_code),
         {},
     )
